251d3d4e7406e32e834ef8c800f4bbd7d027d142
Alex <alexlete29@gmail.com>
2023-02-01 09:31:22 +0100

    Añadir restricción a vista tabla drivers

--- ExamenDWES22-23-main/routes/web.php
+++ ExamenDWES22-23-main/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'enunciado');
 
-Route::get('/admin',[App\Http\Controllers\DriverController::class, 'mostrar'])->name('mostrardrivers');
+Route::get('/admin',[App\Http\Controllers\DriverController::class, 'mostrar'])->name('mostrardrivers')->middleware('auth');
 //Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
